b836261e4fdd07700d381de59c392cad4f68ea32
anOrgandroiD <anorgandroid@outlook.com>
2021-05-14 21:08:45 -0400

    allows drafts to store min/max fighter count

--- database/schemas/draft.js
+++ database/schemas/draft.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 module.exports = mongoose.model("Draft", new mongoose.Schema({
     id: { type: String },
     status: { type: String, default: 'congregating' },
+    minFighters: { type: Number, default: 8 },
+    maxFighters: { type: Number, default: 12 },
     fighterList: { type: [{
         id: { type: String },
         fighterName: { type: String }
