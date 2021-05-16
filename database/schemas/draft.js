1723701e8ae6160041ab6de37a1e926c19051623
anOrgandroiD <anorgandroid@outlook.com>
2021-05-16 12:30:06 -0400

    max vivosaurs + array index of current pick

--- database/schemas/draft.js
+++ database/schemas/draft.js
@@ -5,8 +5,10 @@ module.exports = mongoose.model("Draft", new mongoose.Schema({
     status: { type: String, default: 'congregating' },
     minFighters: { type: Number, default: 8 },
     maxFighters: { type: Number, default: 12 },
+    vivoNum: { type: Number, default: 2 },
     fighterList: { type: [{
         id: { type: String },
         fighterName: { type: String }
-    }]}
+    }]},
+    nextPickNum: { type: Number, default: 0 }
 }));
